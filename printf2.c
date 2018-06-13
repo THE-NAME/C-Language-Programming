02632bbf9e386a9c2399feb6d2905701a5e1de37
THE-NAME <wzk.675@yahoo.com>
2018-06-13 17:37:29 +0800

    更多的printf()的特性

---
+++ printf2.c
@@ -0,0 +1,23 @@
+/*print2.c--更多print()的特性*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+	unsigned int un = 3000000000;//int 为32位，short 为16位的系统
+	short end = 200;
+	long big = 65537;
+	long long verybig = 12345678908642;
+	char grade = '6';
+
+	printf("un = %u and not %d\n", un, un);
+	printf("end=%hd and %d\n", end, end);
+	printf("big=%ld and not %hd\n", big, big);
+	printf("verybig=%lld and not %ld\n", verybig, verybig);
+	printf("%c",grade);
+
+	getchar();
+	return 0;
+
+}
